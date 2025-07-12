e550923baa8a94460a4b4d856e3bc0bfe34f00ca
jyothsna2617 <arunaramu2003@gmail.com>
2025-07-12 12:21:18 +0530

    Final commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,8 @@
-from textblob import TextBlob
 import json
 
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
-    review = body.get('review', '')
-    analysis = TextBlob(review)
-    polarity = analysis.sentiment.polarity
-
-    sentiment = "Neutral"
-    if polarity > 0:
-        sentiment = "Positive"
-    elif polarity < 0:
-        sentiment = "Negative"
-
+    # TODO implement
     return {
         'statusCode': 200,
-        'headers': {"Content-Type": "application/json"},
-        'body': json.dumps({'sentiment': sentiment})
+        'body': json.dumps('Hello from Lambda! From GitHub Actions!')
     }
