f35b280ff989627cf115f4904ab17dde7ee0aecf
jyothsna2617 <arunaramu2003@gmail.com>
2025-07-14 15:10:46 +0530

    changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import random
 import time
 from textblob import TextBlob
 import os
+from mangum import Mangum
 
 app = Flask(__name__)
 CORS(app)
@@ -323,6 +324,7 @@ def health_check():
     """Health check endpoint"""
     return jsonify({'status': 'healthy', 'message': 'Server is running'})
 
+handler = Mangum(app)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
